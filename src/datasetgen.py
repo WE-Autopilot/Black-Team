294169ac45bc9329833d7a196236bfea9c76cad1
Ian Titor <ianpatricktan@gmail.com>
2025-03-19 15:34:34 -0400

    dataset generation should be fully functional now

--- src/datasetgen.py
+++ src/datasetgen.py
@@ -18,18 +18,32 @@ scan_sim = ScanSimulator2D(num_beams, fov)
 
 with hp.File(dataset_path, "w") as file:
 
-    for map_path in map_paths[:1]:
+    for map_path in tqdm(map_paths, desc="Scanning...", unit="map"):
+        map_id = map_path.replace("maps/", "").replace(".yaml", "")
+
+        file.create_group(map_id)
+        file_group = file[map_id]
 
         scan_sim.set_map(map_path, map_ext)
-        csv_path = map_path.replace("maps/", "centerline/").replace(".yaml", ".csv")
+        csv_path = f"centerline/{map_id}.csv"
         waypoints = np.loadtxt(csv_path, delimiter=",")
         path_vecs = np.roll(waypoints, -1, axis=0) - waypoints
 
-        for wp_ind in tqdm(range(0, len(waypoints), interval), desc="Travelling...", unit="waypoint"):
-            theta = np.arctan2(*path_vecs[wp_ind, ::-1])
-            pose = np.append(waypoints[wp_ind], theta)
+        num_waypoints = len(waypoints)
+        num_startpoints = num_waypoints // interval
+
+        file_group.create_dataset("paths", data=path_vecs, chunks=None)
+        file_group.create_dataset("lidar", shape=(num_startpoints, 256, 256), dtype="i8", chunks=None)
+        lidar_dataset = file_group["lidar"]
+
+        for start_ind in range(num_startpoints):
+            waypoint_ind = start_ind * interval
+
+            angle = np.arctan2(*path_vecs[waypoint_ind, ::-1])
+            pose = np.append(waypoints[waypoint_ind], angle)
 
             scan = scan_sim.scan(pose, np.random.default_rng())
             lidar_img = lidar_to_bitmap(scan=scan, channels=1, fov=fov, draw_mode='FILL', bg_color='black', draw_center=False)
             lidar_bitmap = lidar_img / 255
-            Image.fromarray(lidar_img[::-1]).save(f"test/{wp_ind}.png")
+
+            lidar_dataset[start_ind] = lidar_bitmap
