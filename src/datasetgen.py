1f2e40f19bd17612b0b82d3dc114af257aec8e48
Ian Titor <ianpatricktan@gmail.com>
2025-03-20 12:12:20 -0400

    cool args

--- src/datasetgen.py
+++ src/datasetgen.py
@@ -5,15 +5,25 @@ from weap_util.lidar import lidar_to_bitmap
 from glob import glob
 import h5py as hp
 from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--num_beams", type=float, default=1080, help="Number of beams used by lidar scan.")
+parser.add_argument("--fov", type=float, default=2 * np.pi, help="FOV of lidar scan.")
+parser.add_argument("--interval", type=int, default=1, help="Skips by intervals when scanning waypoints.")
+parser.add_argument("--dataset_path", type=string, default="dataset.h5", help="Specify path to save dataset (e.g. dataset.h5).")
+parser.add_argument("--maps_path", type=string, default="./", help="Specify path to read maps (e.g. ./).")
+args = parser.parse_args()
 
 
-num_beams = 1080
-fov = 2 * np.pi
-map_ext = '.png'
-interval = 10
-dataset_path = "dataset.h5"
+num_beams = args.num_beams
+fov = args.fov
+map_ext = args.map_ext
+interval = args.interval
+dataset_path = args.dataset_path
+maps_path = args.maps_path
 
-map_paths = sorted(glob("maps/*.yaml"))
+map_paths = sorted(glob(f"{maps_path}maps/*.yaml"))
 scan_sim = ScanSimulator2D(num_beams, fov)
 
 with hp.File(dataset_path, "w") as file:
@@ -21,13 +31,13 @@ with hp.File(dataset_path, "w") as file:
     file.create_dataset("interval", data=interval)
 
     for map_path in tqdm(map_paths, desc="Scanning...", unit="map"):
-        map_id = map_path.replace("maps/", "").replace(".yaml", "")
+        map_id = map_path.replace(f"{maps_path}maps/", "").replace(".yaml", "")
 
         file.create_group(map_id)
         file_group = file[map_id]
 
         scan_sim.set_map(map_path, map_ext)
-        csv_path = f"centerline/{map_id}.csv"
+        csv_path = f"{maps_path}centerline/{map_id}.csv"
         waypoints = np.loadtxt(csv_path, delimiter=",")
         path_vecs = np.roll(waypoints, -1, axis=0) - waypoints
 
